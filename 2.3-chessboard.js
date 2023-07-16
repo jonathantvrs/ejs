12024cad124237bcf800ae323ad00be22fbb746b
jonathantvrs <jonathan.tavares.silva@ccc.ufcg.edu.br>
2023-07-16 17:48:50 -0300

    exercise ejs 2.3-chessboard solved

--- 2.3-chessboard.js
+++ 2.3-chessboard.js
@@ -18,7 +18,7 @@ let size = 10;
 let board = '';
 for (let x = 0; x < size; x++) {
   for (let y = 0; y < size; y++) {
-  	if ((x + y) % 2 == 0) {
+    if ((x + y) % 2 == 0) {
       board += ' ';
     } else {
       board += '#';
