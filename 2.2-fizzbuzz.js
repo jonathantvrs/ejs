d3368615f18ce32caa56cf20795b575258d54459
jonathantvrs <jonathan.tavares.silva@ccc.ufcg.edu.br>
2023-07-16 16:59:54 -0300

    exercise ejs 2.2-fizzbuzz solved

---
+++ 2.2-fizzbuzz.js
@@ -0,0 +1,15 @@
+// First way
+for (let i = 1; i <= 100; i++) {
+  if (i % 15 == 0) console.log('FizzBuzz');
+  else if (i % 3 == 0) console.log('Fizz');
+  else if (i % 5 == 0) console.log('Buzz');
+  else console.log(i);
+}
+
+// Second way
+for (let n = 1; n <= 100; n++) {
+  let output = "";
+  if (n % 3 == 0) output += "Fizz";
+  if (n % 5 == 0) output += "Buzz";
+  console.log(output || n);
+}
