6d9bd65bc8e13c1c18d0ad0f4c595c8737eee91f
jonathantvrs <jonathan.tavares.silva@ccc.ufcg.edu.br>
2023-07-16 17:45:13 -0300

    exercise ejs 2.3-chessboard solved

---
+++ 2.3-chessboard.js
@@ -0,0 +1,30 @@
+// First 8x8 board
+let output = '';
+for (let line = 0; line < 8; line++) {
+  for (let column = 0; column < 8; column++) {
+  	if ((line + column) % 2 == 0) {
+      output += ' ';
+    } else {
+      output += '#';
+    }
+  }
+  output += '\n';
+}
+
+console.log(output);
+
+// Second nxn board
+let size = 10;
+let board = '';
+for (let x = 0; x < size; x++) {
+  for (let y = 0; y < size; y++) {
+  	if ((x + y) % 2 == 0) {
+      board += ' ';
+    } else {
+      board += '#';
+    }
+  }
+  board += '\n';
+}
+
+console.log(board);
